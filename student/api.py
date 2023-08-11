9f6f13ed2d0f3f270ec9c046a78705ade8aab772
thongnm19089 <62132577+thongnm19089@users.noreply.github.com>
2023-08-12 06:24:19 +0700

    Update api.py

--- student/api.py
+++ student/api.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from .serializer import StudentSerializer
-from .models import Studentddd
+from .models import Student
 class StudentApi(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
